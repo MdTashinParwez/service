2d09d3bfb6b21ddfe4bebb5846930bc8af50e1db
MdTashinParwez <tashinparwez537@gmail.com>
2026-08-13 22:46:02 +0530

    feat: add service API functions for fetching all services and service by ID

---
+++ frontend/src/api/service.api.js
@@ -0,0 +1,39 @@
+import { apiClient } from "./apiClient";
+
+export const getAllServices = async () => {
+  return apiClient("/services/all", {
+    method: "GET",
+  });
+};
+
+export const getServiceById = async (id) => {
+  return apiClient(`/services/${id}`, {
+    method: "GET",
+  });
+};
+
+// export const getMyServices = async () => {
+//   return apiClient("/services/my-services", {
+//     method: "GET",
+//   });
+// };
+
+// export const createService = async (formData) => {
+//   return apiClient("/services", {
+//     method: "POST",
+//     body: formData,
+//   });
+// };
+
+// export const updateService = async (id, formData) => {
+//   return apiClient(`/services/${id}`, {
+//     method: "PATCH",
+//     body: formData,
+//   });
+// };
+
+// export const deleteService = async (id) => {
+//   return apiClient(`/services/${id}`, {
+//     method: "DELETE",
+//   });
+// };
